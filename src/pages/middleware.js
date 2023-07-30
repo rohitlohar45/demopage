ce28a211873ff179f2b71951df9de6857f34599b
Rohit <loharrohit9860@gmail.com>
2023-07-30 16:21:53 +0530

    Backend Added

--- src/pages/middleware.js
+++ src/pages/middleware.js
@@ -1,29 +1,29 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(request) {
-	const path = request.nextUrl.pathname;
+// export function middleware(request) {
+// 	const path = request.nextUrl.pathname;
 
-	// if paths are form /admin/* then isPrivatePath = true
-	const isPrivatePath = path.startsWith("/admin");
+// 	// if paths are form /admin/* then isPrivatePath = true
+// 	const isPrivatePath = path.startsWith("/admin");
 
-	// Get the token from the request cookies
-	const token = request.cookies;
+// 	// Get the token from the request cookies
+// 	const token = request.cookies;
 
-	// If it's a private path and there's no token, redirect to the login page
-	if (isPrivatePath && token != {}) {
-		return NextResponse.redirect("/login");
-	}
+// 	// If it's a private path and there's no token, redirect to the login page
+// 	if (isPrivatePath && token != {}) {
+// 		return NextResponse.redirect("/login");
+// 	}
 
-	// If it's a private path and there is a token, allow the request to continue
-	if (isPrivatePath && token) {
-		return NextResponse.next();
-	}
+// 	// If it's a private path and there is a token, allow the request to continue
+// 	if (isPrivatePath && token) {
+// 		return NextResponse.next();
+// 	}
 
-	// If it's not a private path, allow the request to continue
-	return NextResponse.next();
-}
+// 	// If it's not a private path, allow the request to continue
+// 	return NextResponse.next();
+// }
 
-export const config = {
-	// Add all the paths that need authentication here
-	matcher: ["/admin/:path*"],
-};
+// export const config = {
+// 	// Add all the paths that need authentication here
+// 	matcher: ["/admin/:path*"],
+// };
