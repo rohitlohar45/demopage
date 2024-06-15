94482e7e0b2c26f4f36245318aab14293c23fc23
avalsonigara15 <avals1504@gmail.com>
2024-06-15 18:17:12 +0530

    ~Aval

--- src/pages/projects/[slug].jsx
+++ src/pages/projects/[slug].jsx
@@ -6,19 +6,19 @@ import LightLayout from "../../layouts/light";
 import { WorkTwoColumn } from "../../components/Work-Two-Column";
 
 const WorkPage = () => {
-	const router = useRouter();
-	const { slug } = router.query;
-	return (
-		<LightLayout>
-			<PageHeader
-				title="Architecture is a visual art"
-				fullPath={[]}
-				image="https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-				content="Architecture bibendum pharetra eleifend. Suspendisse vel volutpat purus, sit amet bibendum nisl. Cras mollis turpis a ipsum ultes, nec condimentum ipsum consequat. Mauris vitae consequat nibh, vitae interdum mi."
-			/>
-			<WorkTwoColumn slug={slug} />
-		</LightLayout>
-	);
+  const router = useRouter();
+  const { slug } = router.query;
+  return (
+    <LightLayout>
+      <PageHeader
+        title="Architecture is a visual art"
+        fullPath={[]}
+        image="https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        content="Architecture bibendum pharetra eleifend. Suspendisse vel volutpat purus, sit amet bibendum nisl. Cras mollis turpis a ipsum ultes, nec condimentum ipsum consequat. Mauris vitae consequat nibh, vitae interdum mi."
+      />
+      <WorkTwoColumn slug={slug} />
+    </LightLayout>
+  );
 };
 
 export default WorkPage;
--- src/pages/projects/[slug]/[item].jsx
+++ src/pages/projects/[slug]/[item].jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
 import LightLayout from "../../../layouts/light";
 import PageHeader from "../../../components/Page-header";
@@ -9,71 +8,125 @@ import Services3 from "../../../components/Services3";
 import { useRouter } from "next/router";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../../firebase/firebase";
+import Modal from "react-modal";
+
+Modal.setAppElement("#__next"); // Required for accessibility
 
 const ProjectDetails = () => {
-	const router = useRouter();
-	const [data, setData] = useState({});
-	const { item } = router.query;
+  const router = useRouter();
+  const [data, setData] = useState({});
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+  const { item } = router.query;
 
-	useEffect(() => {
-		// write function to get the data from the firebase of the collection with id of item
-		async function getProjectData() {
-			try {
-				const docRef = doc(db, "projects", item);
-				const docSnap = await getDoc(docRef);
-				if (docSnap.exists()) {
-					setData(docSnap.data());
-					// console.log("Document data:", docSnap.data());
-				} else {
-					// console.log("No such document!");
-				}
-			} catch (error) {
-				console.error("Error getting document:", error);
-			}
-		}
+  useEffect(() => {
+    async function getProjectData() {
+      try {
+        const docRef = doc(db, "projects", item);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setData(docSnap.data());
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.error("Error getting document:", error);
+      }
+    }
 
-		if (item) {
-			getProjectData();
-		}
-	}, [item]);
+    if (item) {
+      getProjectData();
+    }
+  }, [item]);
 
-	return (
-		<LightLayout>
-			{data ? (
-				<>
-					<PageHeader
-						title={data.name}
-						fullPath={[
-							{ id: 2, name: "Works", url: "/work1" },
-							{ id: 3, name: "Work details", url: "/project-details" },
-						]}
-						image={data.thumbnail} // Get the Demo data => Thumbnail
-					/>
-					<ProjectIntro project={data} />
-					<AfterBefore afterImage={data.afterImage} beforeImage={data.beforeImage} />
-					<section className="projdtal" style={{ padding: "20px" }}>
-						<div className="justified-gallery">
-							<div className="row">
-								{data.images?.map((image, index) => (
-									<a key={index} href="#" className="col-lg-4 col-xl-3 col-md-12">
-										<img
-											alt={index}
-											src={image}
-											style={{ objectFit: "contain", height: "200px" }}
-										/>
-									</a>
-								))}
-							</div>
-						</div>
-					</section>
+  const openModal = (image) => {
+    setCurrentImage(image);
+    setModalOpen(true);
+    document.body.classList.add("modal-open");
+  };
 
-					<Services3 />
-				</>
-			) : null}
+  const closeModal = () => {
+    setModalOpen(false);
+    setCurrentImage("");
+    document.body.classList.remove("modal-open");
+  };
 
-			{/* <NextProject /> */}
-		</LightLayout>
-	);
+  return (
+    <LightLayout>
+      {data ? (
+        <>
+          <PageHeader title={data.name} image={data.thumbnail} />
+          <ProjectIntro project={data} />
+          <AfterBefore
+            afterImage={data.afterImage}
+            beforeImage={data.beforeImage}
+          />
+          <section className="projdtal" style={{ padding: "20px" }}>
+            <div className="justified-gallery">
+              <div className="row">
+                {data.images?.map((image, index) => (
+                  <a
+                    key={index}
+                    className="col-lg-4 col-xl-3 col-md-12"
+                    onClick={() => openModal(image)}
+                  >
+                    <img
+                      alt={index}
+                      src={image}
+                      style={{
+                        objectFit: "contain",
+                        height: "200px",
+                        cursor: "pointer",
+                      }}
+                    />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </section>
+          <Services3 />
+        </>
+      ) : null}
+
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            borderRadius: "5px",
+            scrollbarWidth: "none",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            maxWidth: "75%",
+            maxHeight: "85%",
+            padding: 0,
+            border: "none",
+            background: "transparent",
+            zIndex: 1000,
+          },
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.9)",
+            zIndex: 1000,
+          },
+        }}
+      >
+        <div
+          onClick={closeModal}
+          style={{ cursor: "pointer", width: "100%", height: "100%" }}
+        >
+          <img
+            src={currentImage}
+            style={{ width: "100%", height: "auto" }}
+            alt="Popup"
+          />
+        </div>
+      </Modal>
+    </LightLayout>
+  );
 };
 
 export default ProjectDetails;
